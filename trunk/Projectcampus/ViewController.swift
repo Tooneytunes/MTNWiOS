329eb3cfa0a5e6fe71082f4f1be36908b67404ff
Jeroen Zonneveld <jzonneveld@me.com>
2015-03-10 10:11:56 +0100

    Project created

---
+++ trunk/Projectcampus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Projectcampus
+//
+//  Created by Jeroen Zonneveld on 10-03-15.
+//  Copyright (c) 2015 Projectcampus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
