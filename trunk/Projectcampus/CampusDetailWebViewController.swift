d0ab860b45eebd3d807c8d91b44250b310a8222c
Jeroen Zonneveld <jzonneveld@me.com>
2015-03-19 10:06:53 +0100

    Webview toegevoegd voor detail weergave van cursus

---
+++ trunk/Projectcampus/CampusDetailWebViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CampusDetailWebViewController.swift
+//  Projectcampus
+//
+//  Created by Jeroen Zonneveld on 19-03-15.
+//  Copyright (c) 2015 Projectcampus. All rights reserved.
+//
+
+import UIKit
+
+class CampusDetailWebViewController : UIViewController, UIWebViewDelegate {
+    
+    @IBOutlet weak var detailView: UIWebView!
+    
+    override func viewWillAppear(animated: Bool) {
+        title = "iPhone app ontwikkelen"
+    }
+    
+    override func viewDidLoad() {
+        self.detailView.loadRequest(NSURLRequest(URL: NSURL(string: "https://projectcamp.us/projects/iphone-app-ontwikkelen")!))
+    }
+    
+    override func viewDidLayoutSubviews() {
+        detailView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)
+    }    
+}
