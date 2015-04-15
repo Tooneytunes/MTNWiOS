6a9bfcc8a42d668cd56be44f7102d92d90974ac5
Ivan <peppie55@hotmail.com>
2015-04-15 09:36:52 +0200

    Headers toegevoegd

---
+++ Mutating Cells/IOS8SwiftPrototypeCellsTutorial/CustomTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CustomTableViewCell.swift
+//  IOS8SwiftPrototypeCellsTutorial
+//
+//  Created by Kwok Hey Wong on 14/04/15.
+//  Copyright (c) 2015 Arthur Knopper. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var headerLabel: UILabel!
+}
